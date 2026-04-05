2d09a2a6a9f68916ecc9c05ffb07dd220eb68461
linftyz <hi@linfty.com>
2026-04-06 00:45:47 +0800

    feat: add mdx extensions

---
+++ src/components/md/index.ts
@@ -0,0 +1,9 @@
+import Callout from "@/components/md/Callout.astro";
+import Img from "@/components/md/Img.astro";
+import Link from "@/components/md/Link.astro";
+
+export const mdComponents = {
+  a: Link,
+  img: Img,
+  Callout,
+};
